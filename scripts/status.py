812b3421c35c43fe1f1a9390734f1f08c985af54
Byron Henze <byronh@gmail.com>
2015-05-02 12:42:47 -0700

    Switch to i3pystatus

---
+++ scripts/status.py
@@ -0,0 +1,36 @@
+import i3pystatus
+import netifaces
+import os
+import pwd
+import socket
+
+
+status = i3pystatus.Status(standalone=True)
+default_color = '#d8ad4c'
+username = pwd.getpwuid(os.getuid()).pw_name
+hostname = socket.gethostname()
+interfaces = netifaces.interfaces()
+if 'lo' in interfaces:
+    interfaces.remove('lo')
+
+# pip: i3pystatus netifaces colour keyring basiciw
+# aur: python-pywapi
+status.register('alsa')
+status.register('clock', format='%T %p')
+status.register('clock', format='%a %B %d %Y')
+status.register('weather', location_code='CAXX0518')
+status.register('text', text='{}@{}'.format(username, hostname))
+status.register('load', format='load {avg1}')
+status.register('mem',
+                format='mem {percent_used_mem}%',
+                round_size=0,
+                color=default_color)
+status.register('cpu_usage', format='cpu {usage}%')
+for interface in interfaces:
+    status.register('network',
+                    interface=interface,
+                    format_up='{interface}={v4}',
+                    format_down='{interface}',
+                    color_up=default_color)
+
+status.run()
